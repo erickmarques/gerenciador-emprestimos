c3f311a8a6bcf0f1cb08c53acade355a37fc5b7c
Erick Marques <erick.andrade@mv.com.br>
2024-05-20 09:11:15 -0300

    Criando método para converter String para LocalDateTime no utils e colocando mensagem no messages.properties.

--- src/main/java/br/com/gerenciadoremprestimos/util/Utils.java
+++ src/main/java/br/com/gerenciadoremprestimos/util/Utils.java
@@ -1,6 +1,19 @@
 package br.com.gerenciadoremprestimos.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+import java.time.LocalDate;
+
 public class Utils {
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public static LocalDateTime convertStringToLocalDateTime(String dateString) {
+        LocalDate date = LocalDate.parse(dateString, DATE_FORMATTER);
+        return date.atStartOfDay();
+    }
 
     public static boolean contemApenasNumeros(String texto) {
         if (texto == null || texto.isEmpty()) {
@@ -16,5 +29,4 @@ public class Utils {
 
         return true;
     }
-    
 }
